620bd665acc52cc4a7435ce8cd6339b9946e9928
tumeninodes <tumeninodes@gmial.com>
2026-08-08 15:34:26 -0400

    add new light animated balustrade gates and updated related files

--- README.md
+++ README.md
@@ -6,18 +6,21 @@ This mod adds nice looking, decorative balustrades to your world. You can use th
 * **The Materials:** All of the base stone types, clay, and all of the wood types.
 * **Auto-Connecting:** The posts operate like fences using the nodebox type = connected.
 * **Beautiful 3D Previews:** Items use the 3D option for Inventory & Weild Items.
-* **What You Get:** Balustrade Post (the "fence-like" connecting node.
-					Baluster (the straight-line railing).
-					Upper & Lower Balusters (logic should kick in here).
-					And the Stair Balusters (to fill in the gaps between the Upper & Lower nodes).
+* **What You Get:** - Balustrade Post (the "fence-like" connecting node.
+					- Baluster (the straight-line railing).
+					- Upper & Lower Balusters (logic should kick in here).
+					- And the Stair Balusters (to fill in the gaps between the Upper & Lower nodes).
+					- Balustrade Gates: Double gates which are completely modular (you can choose the materials for the railings, & the spindles, changable in the code / init.lua file)
 
 ## How to Get Items
-You can find all the posts & balusters (railings) inside your Creative Mode inventory.
+You can find all the posts, balusters & gates inside your Creative Mode inventory.
 
-If you are playing in Survival Mode, you can make them using standard crafting recipes with your choice of wood or stone materials, ..or add materials from your favorite mods.
+If you are playing in Survival Mode, you can make them using standard crafting recipes with your choice of wood, stone, or metal materials, ..or add materials from your favorite mods.
 
 ![Balustrade Screenshot](Screenshot_01.png)
 
 ![Balustrade Screenshot](Screenshot_02.png)
 
 ![Balustrade Screenshot](Screenshot_03.png)
+
+![Balustrade Screenshot](Screenshot_04.png)
--- init.lua
+++ init.lua
@@ -221,6 +221,99 @@ function balustrade.register_stair_balusters_lower(modname, subname, recipeitem,
 end
 
 
+   -----
+-- Gates
+   -----
+
+function balustrade.register_gate(modname, subname, spindlename, desc, is_wood, gate_spindles_tex)
+	local node_groups = {cracky = 3}
+	local node_sounds = default.node_sound_stone_defaults()
+
+	if is_wood then
+		node_groups = {choppy = 3, oddly_breakable_by_hand = 2}
+		node_sounds = default.node_sound_wood_defaults()
+	end
+
+	local base_groups = {}
+	for k, v in pairs(node_groups) do base_groups[k] = v end
+
+	local base_def = {
+		paramtype = "light",
+		paramtype2 = "facedir",
+		drawtype = "mesh",
+		tiles = {
+			modname .. "_" .. subname .. ".png", -- Slot 1: Railing Mat
+			gate_spindles_tex                    -- Slot 2: Spindle Mat
+		},
+		groups = base_groups,
+		sounds = node_sounds,
+		use_texture_alpha = "clip",
+		sunlight_propagates = true,
+		is_ground_content = false,
+		selection_box = {
+			type = "fixed",
+			fixed = {-8/16, -8/16, -2/16, 8/16, 5/16, 2/16}
+		},
+	}
+
+	local def_closed = {}
+	for k, v in pairs(base_def) do def_closed[k] = v end
+	def_closed.description = desc
+	def_closed.mesh = "balustrade_gate_closed.obj"
+	def_closed.on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+		node.name = "balustrade:" .. subname .. "_gate_" .. spindlename .. "_half"
+		minetest.swap_node(pos, node)
+		minetest.sound_play("doors_fencegate_open", {pos = pos, gain = 0.3, pitch = 1.2}, true)
+
+		minetest.after(0.15, function(p, n)
+			local current_node = minetest.get_node_or_nil(p)
+			if current_node and current_node.name == "balustrade:" .. subname .. "_gate_" .. spindlename .. "_half" then
+				n.name = "balustrade:" .. subname .. "_gate_" .. spindlename .. "_open"
+				minetest.swap_node(p, n)
+			end
+		end, pos, node)
+	end
+	minetest.register_node("balustrade:" .. subname .. "_gate_" .. spindlename .. "_closed", def_closed)
+
+	local def_half = {}
+	for k, v in pairs(base_def) do def_half[k] = v end
+	def_half.description = desc .. " (Opening)"
+	def_half.mesh = "balustrade_gate_half.obj"
+	def_half.groups = {}
+	for k, v in pairs(base_groups) do def_half.groups[k] = v end
+	def_half.groups.not_in_creative_inventory = 1
+	def_half.drop = "balustrade:" .. subname .. "_gate_" .. spindlename .. "_closed"
+
+	minetest.register_node("balustrade:" .. subname .. "_gate_" .. spindlename .. "_half", def_half)
+
+	local def_open = {}
+	for k, v in pairs(base_def) do def_open[k] = v end
+	def_open.description = desc .. " (Open)"
+	def_open.mesh = "balustrade_gate_open.obj"
+	def_open.groups = {}
+	for k, v in pairs(base_groups) do def_open.groups[k] = v end
+	def_open.groups.not_in_creative_inventory = 1
+	def_open.collision_box = { type = "fixed", fixed = {0, 0, 0, 0, 0, 0} }
+	def_open.drop = "balustrade:" .. subname .. "_gate_" .. spindlename .. "_closed"
+
+	def_open.on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+		node.name = "balustrade:" .. subname .. "_gate_" .. spindlename .. "_half"
+		minetest.swap_node(pos, node)
+		minetest.sound_play("doors_fencegate_close", {pos = pos, gain = 0.3, pitch = 1.2}, true)
+
+		minetest.after(0.15, function(p, n)
+			local current_node = minetest.get_node_or_nil(p)
+			if current_node and current_node.name == "balustrade:" .. subname .. "_gate_" .. spindlename .. "_half" then
+				n.name = "balustrade:" .. subname .. "_gate_" .. spindlename .. "_closed"
+				minetest.swap_node(p, n)
+			end
+		end, pos, node)
+	end
+	minetest.register_node("balustrade:" .. subname .. "_gate_" .. spindlename .. "_open", def_open)
+
+end
+
+
    ------------------------
 -- Register Nodes/Materials
    ------------------------
@@ -242,7 +335,7 @@ function balustrade.register_balustrade_nodes(modname, subname, recipeitem, desc
 		}
 	})
 
-	-- Craft: Regular Balusters
+	-- Craft: Straight Balusters
 	minetest.register_craft({
 		output = "balustrade:" .. subname .. "_balusters 6",
 		recipe = {
@@ -283,6 +376,7 @@ function balustrade.register_balustrade_nodes(modname, subname, recipeitem, desc
 	})
 end
 
+-- Base Balustrade Registrations
 balustrade.register_balustrade_nodes("default", "clay", "default:clay", "Clay", false)
 balustrade.register_balustrade_nodes("default", "desert_sandstone", "default:desert_sandstone", "Desert Sandstone", false)
 balustrade.register_balustrade_nodes("default", "desert_stone", "default:desert_stone", "Desert Stone", false)
@@ -296,9 +390,33 @@ balustrade.register_balustrade_nodes("default", "pine_wood", "default:pine_wood"
 balustrade.register_balustrade_nodes("default", "acacia_wood", "default:acacia_wood", "Acacia Wood", true)
 balustrade.register_balustrade_nodes("default", "aspen_wood", "default:aspen_wood", "Aspen Wood", true)
 
+-- Custom Gate Registrations
+balustrade.register_gate("default", "sandstone", "obsidian", "Sandstone Gate (Obsidian Spindles)", false, "default_obsidian.png")
+balustrade.register_gate("default", "stone", "wood", "Stone Gate (Wood Spindles)", false, "default_wood.png")
+balustrade.register_gate("default", "wood", "tinblock", "Wooden Gate (Tin Spindles)", true, "default_tin_block.png")
+balustrade.register_gate("default", "junglewood", "copperblock", "Junglewood Gate (Copper Spindles)", true, "default_copper_block.png")
+
+-- Gate Crafting
+minetest.register_craft({
+	output = "balustrade:sandstone_gate_obsidian_closed 1",
+	recipe = {
+		{"default:sandstone", "default:obsidian", "default:sandstone"},
+		{"default:sandstone", "default:sandstone", "default:sandstone"},
+	}
+})
+
+minetest.register_craft({
+	output = "balustrade:wood_gate_tin_closed 1",
+	recipe = {
+		{"default:wood", "default:tin_block", "default:wood"},
+		{"default:wood", "default:wood",        "default:wood"},
+	}
+})
+
 --Example-- to add your favorite mod's nodes
 --[[
 if minetest.get_modpath("granite") then
 	balustrade.register_balustrade_nodes("granite", "granite", "granite:granite", "Granite", false)
 end
 ]]--
+
